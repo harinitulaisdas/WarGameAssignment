27202558a92ce3fd98dfcfea4c2404f2a874325e
Harini <harinitulasidas@gmail.com>
2021-11-21 20:26:51 -0800

    Refractored to improved readability. Added documentation

--- main.py
+++ main.py
@@ -1,82 +1,125 @@
+"""
+The war card game using python for Intel Interview.
+
+Author:Harini Gowdagere Tulaisdas
+
+Assumptions:
+Suits are ignored.
+The card values are used for comparison.
+The top card of the deck is represented by the left most element of the list.
+Eg : Player 1 ['A','5','4'] :
+     'A' is considered as the card at the top of the deck.
+
+During each battle.
+"""
 import random
 
-war_cards = []
-Player1Deck = []
-Player2Deck = []
-# Player1Deck = ['3','6','4', '4','2', '3','6','4']
-# Player2Deck = ['9','2','4', '4','8', '9','K','4']
-card_values = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q', 'A')
-winner = ""
-war = False
+CARD_VALUES = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q', 'A')
+
 
 def PrepareDeck():
     """
     Generates the deck of cards, shuffles it before dealing them to the two players
     """
-    deck = [val for val in card_values for i in range(4)]
+    deck = [val for val in CARD_VALUES for i in range(4)]
     random.shuffle(deck)
+    player1_deck, player2_deck = [], []
     for i in range(26):
-        Player1Deck.append(deck.pop())
-        Player2Deck.append(deck.pop())
+        player1_deck.append(deck.pop(0))
+        player2_deck.append(deck.pop(0))
+    return player1_deck, player2_deck
 
 
-def CompareCards():
-    global war
-    status()
-    card1 = Player1Deck.pop(0)
-    card2 = Player2Deck.pop(0)
-    print("The cards played by each player : ")
-    print("Player 1  :  ", card1)
-    print("Player 2  :  ", card2)
-    game_cards = []
-    game_cards.append(card1)
-    game_cards.append(card2)
-    if card_values.index(card1) == card_values.index(card2):
-        print("at war")
-        war = True
-        war_cards.extend(game_cards)
-        for i in range(3):
-            war_cards.append(Player1Deck.pop(0))
-            war_cards.append(Player2Deck.pop(0))
-        print("war cards : ", war_cards)
-    else:
-        if card_values.index(card1) > card_values.index(card2):
-            print("player 1 wins the round!!")
-            print("value of war ", war)
-            if war:
-                Player1Deck.extend(war_cards)
-                war_cards.clear()
-                war = False
-            Player1Deck.extend(game_cards)
-        else:
-            print("player 2 wins the round!!")
-            print("game cards: ", game_cards)
-            if war:
-                Player2Deck.extend(war_cards)
-                war_cards.clear()
-                war = False
-            Player2Deck.extend(game_cards)
+def DecideWinner():
+    """
+    The method to decide the winner of the game. The player with more cards on hand is declared the winner.
+    :return:
+    """
+    print('*' * 20, "WAR ENDS!!", '*' * 23)
+    print("Players have exhausted all the cards")
+    print("Player 1 has :", len(player1_deck), " cards")
+    print("Player 2 has :", len(player2_deck), " cards")
+    winner = "Player 1" if len(player1_deck) > len(player2_deck) else "Player 2 "
+    print("The winner is :", winner)
+    print('*' * 55)
 
-
-def status():
-    print("Player 1 deck :")
-    print(Player1Deck)
-    print("Player 2 deck :")
-    print(Player2Deck)
-    print("Player 1 has :", len(Player1Deck), " cards")
-    print("Player 2 has :", len(Player2Deck), " cards")
 
 def WarGame():
     """
+    This method plays the game of war.
+
+    During a round of game, each Player plays one card and the player who has the card with higher value gets all the cards. If both the cards are of same value,
+    then there is a war. In case of a war, both the players place the next three cards face down and another card face-up. The player with
+    higher face-up card wins the war and gets all the 10 cards.
+
+    Entry condition: The game is played when each player has atleast 4 cards since each player needs 4 cards during a war(3 face donw and one face up)
+
+    winning criteria: The player with more cards on hand will be declared the winner. The cards on table arent considered during the
+    decision.
+
+    The game is played when both the players have atleast 4 cards each. This is to ensure that
+    the player has enough cards in case of a war situation, i.e. face down 3 cards and face up one card.
+
+    """
+    while len(player1_deck) > 4 and len(player2_deck) > 4:
+        global war
+        card1 = player1_deck.pop(0)
+        card2 = player2_deck.pop(0)
+        print('*' * 20, "New Battle", '*' * 23)
+        print("The cards played by each player : ")
+        print("Player 1  :  ", card1)
+        print("Player 2  :  ", card2)
+        print('*' * 55)
+        # game_cards [] - list to hold the cards played by each player at every battle/round of game.
+        game_cards = []
+        game_cards.append(card1)
+        game_cards.append(card2)
+        # check if there is a war
+        if CARD_VALUES.index(card1) == CARD_VALUES.index(card2):
+            print('*' * 17, "Players are at WAR!!", '*' * 17)
+            war = True
+            war_cards.extend(game_cards)
+            for i in range(3):
+                war_cards.append(player1_deck.pop(0))
+                war_cards.append(player2_deck.pop(0))
+        else:
+            if CARD_VALUES.index(card1) > CARD_VALUES.index(card2):
+                print('*' * 3, "Player 1 wins the round and gets all the cards!!", '*' * 3)
+                if war:
+                    player1_deck.extend(war_cards)
+                    war_cards.clear()
+                    war = False
+                player1_deck.extend(game_cards)
+            else:
+                print("Player 2 wins the round and gets all the cards!!")
+                if war:
+                    player2_deck.extend(war_cards)
+                    war_cards.clear()
+                    war = False
+                player2_deck.extend(game_cards)
+
+
+def Status():
+    """
+    Utility method to know the cards on hand and on the table during the game.
     :return:
     """
-    while len(Player1Deck) > 3 and len(Player2Deck) > 3:
-        CompareCards()
+    print("Player 1 deck :")
+    print(player1_deck)
+    print("Player 2 deck :")
+    print(player2_deck)
+    print("war cards/cards on the table: ", war_cards)
+    print("Player 1 has :", len(player1_deck), " cards")
+    print("Player 2 has :", len(player2_deck), " cards")
+
 
 if __name__ == '__main__':
-    """
-    Pile of cards generated by each player during the game. 
-    """
-    """ The cards dealt to each player"""
-    PrepareDeck()
+    print("******************Welcome to the game of WAR*********************************")
+    # war_cards : hold the cards on the table during a war
+    war_cards = []
+    # deck of cards for each player
+    player1_deck, player2_deck = PrepareDeck()
+    # war : to hold the status if there was a war.
+    war = False
     WarGame()
+    DecideWinner()
